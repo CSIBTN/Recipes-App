5536a6b32a2cc7e78fee0bead2a48db41ac65dea
CSIBTN <krutinivan3@gmail.com>
2023-02-20 11:56:57 +0200

    Added details view and hyperlinks to the recipes

--- app/src/main/java/com/csibtn/recipehub/ui/RecipeDetailsFragment.kt
+++ app/src/main/java/com/csibtn/recipehub/ui/RecipeDetailsFragment.kt
@@ -1,13 +1,19 @@
 package com.csibtn.recipehub.ui
 
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.csibtn.recipehub.R
 import com.csibtn.recipehub.databinding.FragmentRecipeDetailsBinding
+import java.util.*
 
 class RecipeDetailsFragment : Fragment() {
     private val recipeArg : RecipeDetailsFragmentArgs by navArgs()
@@ -28,8 +34,20 @@ class RecipeDetailsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("Received recipe data : ", "${recipeArg.recipe}")
         super.onViewCreated(view, savedInstanceState)
+        val recipeLink = "<a href=\"${recipeArg.recipe.sourceUrl}\">Check out this recipe here!</a>"
+        Glide.with(requireContext())
+            .load(recipeArg.recipe.image)
+            .into(recipeDetailsBinding.recipeIconIV)
+        recipeDetailsBinding.recipeNameTV.text = recipeArg.recipe.name
+        recipeDetailsBinding.likesTV.text = "${recipeArg.recipe.likes}"
+        recipeDetailsBinding.healthScoreTV.text = "${recipeArg.recipe.healthScore}"
+        recipeDetailsBinding.timeToPrepareTV.text = "${recipeArg.recipe.timeToPrepare}"
+        recipeDetailsBinding.summaryTV.text = recipeArg.recipe.summary
+        recipeDetailsBinding.recipeLinkTV.text = Html.fromHtml(recipeLink,HtmlCompat.FROM_HTML_MODE_COMPACT)
+        recipeDetailsBinding.recipeLinkTV.movementMethod = LinkMovementMethod.getInstance()
+        recipeDetailsBinding.summaryTV.text = Html.fromHtml(recipeArg.recipe.summary,HtmlCompat.FROM_HTML_MODE_COMPACT)
+        recipeDetailsBinding.summaryTV.movementMethod = LinkMovementMethod.getInstance()
     }
 
     override fun onDestroy() {
